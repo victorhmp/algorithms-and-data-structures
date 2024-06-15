d2d9a334b4c0ba810ee1df2f13e578b349b82b0b
Victor Hugo M. Pinto <victor2142@gmail.com>
2024-06-15 14:49:13 -0300

    Initial commit

---
+++ python/heap.py
@@ -0,0 +1,12 @@
+class Heap:
+    def __main__(self):
+       self.property = 0
+       return
+    def insert(self, value):
+        return
+
+    def delete(self):
+        return
+
+    def heapify(self):
+        return
---
+++ python/heapsort.py
@@ -0,0 +1,3 @@
+# Heapsort is another sorting algorithm that can run on O(n log n) time.
+# Different from quicksort, heapsort sorts *in place*, so only a constant
+# number of array elements are stored outside the input array at any time.
